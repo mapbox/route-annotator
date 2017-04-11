547b4abda6f8684a0196e989548e3eed7c76abfb
Kajari <kajari@mapbox.com>
2017-04-11 14:45:25 -0700

    get tests to pass

--- src/hashmap.hpp
+++ src/hashmap.hpp
@@ -1,6 +1,37 @@
 #include <fstream>
 #include <iostream>
 #include "types.hpp"
+#include <sparsepp/spp.h>
+
+using spp::sparse_hash_map;
+
+struct Way 
+{
+    bool operator==(const Way &o) const 
+    { return to == o.to && from == o.from; }
+
+    Way(int to, int from) : to(to), from(from) { }
+
+    int to;
+    int from;
+};
+
+namespace std
+{
+    // inject specialization of std::hash for Way into namespace std
+    // ----------------------------------------------------------------
+    template<> 
+    struct hash<Way>
+    {
+        std::size_t operator()(Way const &p) const
+        {
+            std::size_t seed = 0;
+            spp::hash_combine(seed, p.to);
+            spp::hash_combine(seed, p.from);
+            return seed;
+        }
+    };
+}
 
 class Hashmap {
     // TODO: turn into private
@@ -11,4 +42,6 @@ class Hashmap {
     public:
         void loadData(std::ifstream input);
         speed_t getValue(external_nodeid_t to, external_nodeid_t from);
+
+    sparse_hash_map<Way, int> annotations;
 };
