b344695eafd1554200e9cb6f05ee8b7cfbfe178d
Kajari <kajari@mapbox.com>
2017-04-11 13:20:24 -0700

    skeleton hashmap

---
+++ src/hashmap.cpp
@@ -0,0 +1,7 @@
+#include "hashmap.hpp"
+
+    void Hashmap::add(external_nodeid_t to, external_nodeid_t from, speed_t speed) {};
+    bool Hashmap::hasKey(external_nodeid_t to, external_nodeid_t from) { return true;};
+
+    void Hashmap::loadData(std::ifstream input) {};
+    speed_t Hashmap::getValue(external_nodeid_t to, external_nodeid_t from){ return 0;};
