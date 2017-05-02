c285faf83bb8f48bd126f63676f33b49630e6e2f
Kajari <kajari@mapbox.com>
2017-05-02 16:29:29 -0400

    couple more tests

--- test/congestion/congestion.cpp
+++ test/congestion/congestion.cpp
@@ -28,16 +28,19 @@ BOOST_AUTO_TEST_CASE(congestion_test_many)
 	BOOST_CHECK_EQUAL(hm.getValue(86623322,86622998),89);
 	BOOST_CHECK_EQUAL(hm.getValue(86622998,86623322),88);
 	BOOST_CHECK_EQUAL(hm.getValue(86909074,86909072),79);
+}
+
+BOOST_AUTO_TEST_CASE(congestion_test_get_values)
+{
+    Hashmap hm("test/congestion/fixtures/congestion.csv");
 
 	std::vector<external_nodeid_t> nodes{86909055, 86909053, 86909050, 86909053, 86909055};
-
 	std::vector<congestion_speed_t> speeds{81, 81, 81, 81};
-
 	std::vector<congestion_speed_t> response = hm.getValues(nodes);
-
 	BOOST_CHECK_EQUAL_COLLECTIONS(response.begin(), response.end(), speeds.begin(), speeds.end());
 }
 
+// Execptions
 BOOST_AUTO_TEST_CASE(congestion_test_string_input)
 {
     BOOST_CHECK_THROW(Hashmap hm("test/congestion/fixtures/string_input.csv"), std::exception);
@@ -84,13 +87,18 @@ BOOST_AUTO_TEST_CASE(congestion_test_many_exceptions)
 	BOOST_CHECK_THROW(hm.getValue(86623122,86620998), std::exception);
 	BOOST_CHECK_THROW(hm.getValue(86600998,86620322), std::exception);
 	BOOST_CHECK_THROW(hm.getValue(86900074,86900072), std::exception);
+}
 
-	std::vector<external_nodeid_t> nodes{86909050, 86900053, 86900050};
+BOOST_AUTO_TEST_CASE(congestion_test_get_values_exceptions)
+{
+    Hashmap hm("test/congestion/fixtures/congestion.csv");
 
+	std::vector<external_nodeid_t> nodes{86909055};
+	BOOST_CHECK_THROW(hm.getValues(nodes), std::exception);
+
+	std::vector<external_nodeid_t> nodes2{86909050, 86900053, 86900050};
 	std::vector<congestion_speed_t> speeds{INVALID_SPEED, INVALID_SPEED};
-
-	std::vector<congestion_speed_t> response = hm.getValues(nodes);
-
+	std::vector<congestion_speed_t> response = hm.getValues(nodes2);
 	BOOST_CHECK_EQUAL_COLLECTIONS(response.begin(), response.end(), speeds.begin(), speeds.end());
 }
 
