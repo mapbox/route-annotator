42bf3bc32c195f3b0b726eb28c4d3c9a321cfed3
Greg Knisely <greg.knisely@mapbox.com>
2019-03-13 11:13:16 -0400

    added tests for records with units.

--- test/congestion/congestion.cpp
+++ test/congestion/congestion.cpp
@@ -28,14 +28,19 @@ BOOST_AUTO_TEST_CASE(congestion_test_many)
     BOOST_CHECK_EQUAL(map.getValue(86623322, 86622998), 89);
     BOOST_CHECK_EQUAL(map.getValue(86622998, 86623322), 88);
     BOOST_CHECK_EQUAL(map.getValue(86909074, 86909072), 79);
+    BOOST_CHECK_EQUAL(map.getValue(297977101, 312122762), 5);
+    BOOST_CHECK_EQUAL(map.getValue(69395079,69402983), 23);
+    BOOST_CHECK_EQUAL(map.getValue(3860306483,1362215135), 6);
+    BOOST_CHECK_EQUAL(map.getValue(1362215135,297976455), 10);
+
 }
 
 BOOST_AUTO_TEST_CASE(congestion_test_get_values)
 {
     SegmentSpeedMap map("test/congestion/fixtures/congestion.csv");
 
-    std::vector<external_nodeid_t> nodes{86909055, 86909053, 86909050, 86909053, 86909055};
-    std::vector<segment_speed_t> speeds{81, 81, 81, 81};
+    std::vector<external_nodeid_t> nodes{86909055, 86909053, 86909050, 86909053, 86909055, 3860306483, 1362215135, 297976455};
+    std::vector<segment_speed_t> speeds{81, 81, 81, 81,INVALID_SPEED,6,10};
     std::vector<segment_speed_t> response = map.getValues(nodes);
     BOOST_CHECK_EQUAL_COLLECTIONS(response.begin(), response.end(), speeds.begin(), speeds.end());
 }
@@ -50,13 +55,13 @@ BOOST_AUTO_TEST_CASE(congestion_load_multiple)
                                       "test/congestion/fixtures/congestion2.csv"};
     SegmentSpeedMap map(paths);
 
-    nodes = {86909055, 86909053, 86909050, 86909053, 86909055};
-    expected = {81, 81, 81, 81};
+    nodes = {86909055, 86909053, 86909050, 86909053, 86909055, 3860306483, 1362215135, 297976455};
+    expected = {81, 81, 81, 81,INVALID_SPEED,6,10};
     actual = map.getValues(nodes);
     BOOST_CHECK_EQUAL_COLLECTIONS(actual.begin(), actual.end(), expected.begin(), expected.end());
 
-    nodes = {90, 91, 92, 93, 94};
-    expected = {2, 3, 4, 5};
+    nodes = {90, 91, 92, 93, 94, 297976455};
+    expected = {2, 3, 4, 5, 10};
     actual = map.getValues(nodes);
     BOOST_CHECK_EQUAL_COLLECTIONS(actual.begin(), actual.end(), expected.begin(), expected.end());
 }
@@ -70,20 +75,20 @@ BOOST_AUTO_TEST_CASE(congestion_load_incremental)
     std::vector<std::string> paths = {"test/congestion/fixtures/congestion.csv"};
     SegmentSpeedMap map(paths);
 
-    nodes = {86909055, 86909053, 86909050, 86909053, 86909055};
-    expected = {81, 81, 81, 81};
+    nodes = {86909055, 86909053, 86909050, 86909053, 86909055, 3860306483, 1362215135, 297976455};
+    expected = {81, 81, 81, 81,INVALID_SPEED,6,10};
     actual = map.getValues(nodes);
     BOOST_CHECK_EQUAL_COLLECTIONS(actual.begin(), actual.end(), expected.begin(), expected.end());
 
-    nodes = {90, 91, 92, 93, 94};
-    expected = {INVALID_SPEED, INVALID_SPEED, INVALID_SPEED, INVALID_SPEED};
+    nodes = {90, 91, 92, 93, 94, 297976455};
+    expected = {INVALID_SPEED, INVALID_SPEED, INVALID_SPEED, INVALID_SPEED, INVALID_SPEED};
     actual = map.getValues(nodes);
 
     BOOST_CHECK_EQUAL_COLLECTIONS(actual.begin(), actual.end(), expected.begin(), expected.end());
 
     map.loadCSV("test/congestion/fixtures/congestion2.csv");
 
-    expected = {2, 3, 4, 5};
+    expected = {2, 3, 4, 5, 10};
     actual = map.getValues(nodes);
 
     BOOST_CHECK_EQUAL_COLLECTIONS(actual.begin(), actual.end(), expected.begin(), expected.end());
@@ -94,11 +99,15 @@ BOOST_AUTO_TEST_CASE(congestion_test_string_input)
 {
     BOOST_CHECK_THROW(SegmentSpeedMap map("test/congestion/fixtures/string_input.csv"),
                       std::exception);
+    BOOST_CHECK_THROW(SegmentSpeedMap map("test/congestion/fixtures/string_input2.csv"),
+                      std::exception);
 }
 
 BOOST_AUTO_TEST_CASE(congestion_test_more_columns)
 {
     BOOST_CHECK_THROW(SegmentSpeedMap map("test/congestion/fixtures/more_columns.csv"),
+                      std::exception);
+    BOOST_CHECK_THROW(SegmentSpeedMap map("test/congestion/fixtures/more_columns2.csv"),
                       std::exception);
 }
 
@@ -106,11 +115,15 @@ BOOST_AUTO_TEST_CASE(congestion_test_fewer_columns)
 {
     BOOST_CHECK_THROW(SegmentSpeedMap map("test/congestion/fixtures/fewer_columns.csv"),
                       std::exception);
+    BOOST_CHECK_THROW(SegmentSpeedMap map("test/congestion/fixtures/fewer_columns2.csv"),
+                      std::exception);
 }
 
 BOOST_AUTO_TEST_CASE(congestion_test_negative_number)
 {
     BOOST_CHECK_THROW(SegmentSpeedMap map("test/congestion/fixtures/negative_number.csv"),
+                      std::exception);
+    BOOST_CHECK_THROW(SegmentSpeedMap map("test/congestion/fixtures/negative_number2.csv"),
                       std::exception);
 }
 
@@ -118,16 +131,21 @@ BOOST_AUTO_TEST_CASE(congestion_test_invalid_csv)
 {
     BOOST_CHECK_THROW(SegmentSpeedMap map("test/congestion/fixtures/invalid_csv.csv"),
                       std::exception);
+    BOOST_CHECK_THROW(SegmentSpeedMap map("test/congestion/fixtures/invalid_csv2.csv"),
+                      std::exception);
 }
 
 BOOST_AUTO_TEST_CASE(congestion_test_header)
 {
     BOOST_CHECK_THROW(SegmentSpeedMap map("test/congestion/fixtures/header.csv"), std::exception);
+    BOOST_CHECK_THROW(SegmentSpeedMap map("test/congestion/fixtures/header2.csv"), std::exception);
 }
 
 BOOST_AUTO_TEST_CASE(congestion_test_not_a_number)
 {
     BOOST_CHECK_THROW(SegmentSpeedMap map("test/congestion/fixtures/not_a_number.csv"),
+                      std::exception);
+    BOOST_CHECK_THROW(SegmentSpeedMap map("test/congestion/fixtures/not_a_number2.csv"),
                       std::exception);
 }
 
@@ -142,6 +160,10 @@ BOOST_AUTO_TEST_CASE(congestion_test_many_exceptions)
     BOOST_CHECK_THROW(map.getValue(86623122, 86620998), std::exception);
     BOOST_CHECK_THROW(map.getValue(86600998, 86620322), std::exception);
     BOOST_CHECK_THROW(map.getValue(86900074, 86900072), std::exception);
+    BOOST_CHECK_THROW(map.getValue(207977101, 312122762), std::exception);
+    BOOST_CHECK_THROW(map.getValue(63395079,69402983), std::exception);
+    BOOST_CHECK_THROW(map.getValue(3560306483,1362215135), std::exception);
+    BOOST_CHECK_THROW(map.getValue(1362215135,297076455), std::exception);
 }
 
 BOOST_AUTO_TEST_CASE(congestion_test_get_values_exceptions)
@@ -151,8 +173,11 @@ BOOST_AUTO_TEST_CASE(congestion_test_get_values_exceptions)
     std::vector<external_nodeid_t> nodes{86909055};
     BOOST_CHECK_THROW(map.getValues(nodes), std::exception);
 
-    nodes = {86909050, 86900053, 86900050};
-    std::vector<segment_speed_t> expected{INVALID_SPEED, INVALID_SPEED};
+    nodes = {297076455};
+    BOOST_CHECK_THROW(map.getValues(nodes), std::exception);
+
+    nodes = {86909050, 86900053, 86900050, 297076455};
+    std::vector<segment_speed_t> expected{INVALID_SPEED, INVALID_SPEED, INVALID_SPEED};
     std::vector<segment_speed_t> actual = map.getValues(nodes);
     BOOST_CHECK_EQUAL_COLLECTIONS(actual.begin(), actual.end(), expected.begin(), expected.end());
 }
